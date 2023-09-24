4e6ada742f63b62dbd8c0c0c0113679120548101
joaomn <97440058+joaomn@users.noreply.github.com>
2023-09-24 12:21:05 -0300

    novas features

--- assets/js/features.js
+++ assets/js/features.js
@@ -1,45 +1,55 @@
-// biblioteca de manipulação de dom <script src= "https://code.jquery.com/jquery-3.7.1.min.js"></script> (incuir no html principal)
 
-
-
-fetch('http://api.weatherstack.com/autocomplete' + "?access_key=33a4d331f8f5920713fd707d04cb4dae" 
-+ "&query=" + "london")
-   
- //criando a promesse
-.then((resposta)=>{ //cria um objeto dentrod e uma função
-    return resposta.json() //pega apenas o json deste objeto
-})
-.then((data) =>{ //tratamento e manipulação do dom atravez das infromações do objeto
-
-      
-console.log(data)
-})
-.catch((qlqrnome)=>{
-console.log(" o erro foi causado por " + qlqrnome)
-})// retorna um objeto com os dados do usuário
-
-
-
-
-
-
-function obterReceita(receita){
-fetch('https://api.edamam.com/api/recipes/v2?' + "type=public" + '&q=' + receita + '&app_id=89e2db89' 
-+ '&app_key=b2845d8c0787787706ecc19951a4d390' + '&ingr=1-10')
-.then((resp)=>{
-    return resp.json();
-})
-.then((data)=>{
-   
-   // $('#fototeste').attr("src",data.hits.recives.image ) 
+async function obter(palavra){
+    $('#translated').empty()
+    const url = 'https://google-translate1.p.rapidapi.com/language/translate/v2';
+const options = {
+	method: 'POST',
+	headers: {
+		'content-type': 'application/x-www-form-urlencoded',
+		'Accept-Encoding': 'application/gzip',
+		'X-RapidAPI-Key': 'eac3dc7f3emsh577f3262e597fd1p165d55jsn1ea4fe9ce59e',
+		'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+	},
+	body: new URLSearchParams({
+		q: palavra,
+		target: 'ja',
+		source: 'pt'
+	})
     
-   
-    console.log(data)
-})
-.catch((err)=>{
-    console.log(err)
-})
+};
 
+try {
+	const div = document.getElementById("divResposta");
+	const response = await fetch(url, options);	
+	const result =  await response.json();
 
-
+	
+    $('#translated').append("O Kanji Correspondente a " + palavra + " é: " + result.data.translations[0].translatedText)
+	console.log(result);
+	if(div.innerHTML.trim() === ""){
+		
+	}else{
+		div.style.display= "block";
+	}
+} catch (error) {
+	console.error(error);
 }
+}
+
+// funcao de calcular distancia
+
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+	var R = 6371; // Radius of the earth in km
+	var dLat = deg2rad(lat2-lat1);  // deg2rad below
+	var dLon = deg2rad(lon2-lon1); 
+	var a = 
+	  Math.sin(dLat/2) * Math.sin(dLat/2) +
+	  Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+	  Math.sin(dLon/2) * Math.sin(dLon/2)
+	  ; 
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+	var d = R * c; // Distance in km
+	return d;
+  }
+  
+ 
